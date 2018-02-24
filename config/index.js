0489b03e5d31d91fb85789b85070e4e38c47c64f
Julien Bisconti <julienbi@unity3d.com>
2018-02-24 22:31:39 +0200

    Initial commit

---
+++ config/index.js
@@ -0,0 +1,5 @@
+const secretOrKey = process.env.SECRET || '0nLy4D3vM0f0';
+
+module.exports = {
+  secretOrKey,
+};
---
+++ index.js
@@ -0,0 +1,4 @@
+// require = require('@std/esm')(module); // eslint-disable-line
+// module.exports = require('./lib/index.js').default;
+
+require('./lib');
